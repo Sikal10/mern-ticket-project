8ebb5753b90ad5a32602a77bce843deca5721069
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-19 20:58:43 +0100

    user signup and login controllers

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,16 +1,44 @@
+import User from "../models/User.js";
+import {comparePassword, hashPasswordHandler, validateUserData} from "../helpers/validate.js";
+import asyncHandler from "express-async-handler";
+import {errorResponse} from "../helpers/errorResponse.js";
+import {generateAccessToken} from "../helpers/token.js";
 
 /** @desc register a user
- *  @route /api/auth/register
+ *  @route /api/auth/register POST
  *  @access public
  * */
-export const registerUser = async (req, res) => {
-    res.status(200).json({message: "User created"});
-};
+export const registerUser = asyncHandler(async (req, res, next) => {
+    const {name, email, password} = req.body;
+
+    /** check if the credentials are valid  */
+    validateUserData(name, email, password, res);
+
+    /** check if there is an existing user */
+    const existingUser = await User.findOne({email});
+    if (existingUser) return next(errorResponse(404, `user with email ${email} already exists`));
+
+    /** hash the password before saving to database */
+    const hashedPassword = await hashPasswordHandler(password);
+
+    const user = await User.create({name, email, password: hashedPassword});
+    res.status(201).json({success: true, user});
+});
 
 /** @desc login a user
- *  @route /api/auth/login
+ *  @route /api/auth/login POST
  *  @access public
  * */
-export const loginUser = async (req, res) => {
-    console.log("user logged in");
-};
+export const loginUser = asyncHandler(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    /**check if user already exists*/
+    const user = await User.findOne({email});
+    if (!user) return next(errorResponse(404, "user not found"));
+
+    /**check if the password is correct*/
+    const isPasswordCorrect = await comparePassword(password, user);
+    if (!isPasswordCorrect) return next(errorResponse(400, "Invalid credentials"));
+
+    res.status(200).json({success: true, user});
+});
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import {errorHandler} from "./middlewares/errorMiddleware.js";
 
 /** import all routes */
 import authRoutes from "../backend/routes/authRoutes.js";
+
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -10,6 +16,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+
+
+app.use(errorHandler);
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080")
