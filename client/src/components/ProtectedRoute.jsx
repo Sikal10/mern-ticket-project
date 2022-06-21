5c5e4546f4ad65476facfafe6ebba046a5ae065d
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-21 21:37:58 +0100

    Authorization in the Protected Route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,20 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {useAuthStatus} from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
 
-const ProtectedRoute = () => {
-    const {checkingStatus, isAuthenticated} = useAuthStatus();
+const ProtectedRoute = ({role}) => {
+    const {checkingStatus, isAuthenticated, userRole} = useAuthStatus();
 
     if (checkingStatus) {
-        return <h2>Loading...</h2>
+        return <Spinner/>
     }
 
-    return isAuthenticated ? <Outlet /> : <Navigate to={"/login"} />
+    if (isAuthenticated && role) {
+        return isAuthenticated && role === userRole ? <Outlet/> : <Navigate to={"/unauthorized"}/>
+    } else {
+        return isAuthenticated ? <Outlet/> : <Navigate to={"/login"}/>
+    }
+
 
 };
 
--- client/src/hooks/useAuthStatus.js
+++ client/src/hooks/useAuthStatus.js
@@ -5,17 +5,20 @@ import {selectAuthUser} from "../redux/slices/authSlice/authSlice";
 export const useAuthStatus = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [checkingStatus, setCheckingStatus] = useState(true);
+    const [userRole, setUserRole] = useState(false);
 
     const {user} = useSelector(selectAuthUser);
 
     useEffect(() => {
         if (user) {
             setIsAuthenticated(true);
+            setUserRole(user.isAdmin);
         } else {
             setIsAuthenticated(false);
+            setUserRole(false);
         }
         setCheckingStatus(false);
     }, [user]);
 
-    return {isAuthenticated, checkingStatus}
+    return {isAuthenticated, checkingStatus, userRole}
 }
