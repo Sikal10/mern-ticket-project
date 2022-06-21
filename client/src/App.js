7a162708f0e285a387bb54d7d4ffb522651e382a
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-21 21:39:09 +0100

    Authorization in the Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,9 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import NewTicket from "./pages/NewTicket";
 import Tickets from "./pages/Tickets";
 import Ticket from "./pages/Ticket";
+import Users from "./pages/Users";
 import {ToastContainer} from "react-toastify";
+import AccessDenied from "./components/AccessDenied";
 
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -17,25 +19,34 @@ function App() {
   return (
     <div className={"bg-[#F6F9FF] h-screen"}>
         <Router>
-            <div className={"max-w-7xl px-[30px] mx-auto"}>
+            <div className={"max-w-6xl px-[30px] mx-auto"}>
                 <Header />
             </div>
 
-            <div className={"mt-[30px] max-w-7xl mx-auto px-[30px]"}>
+            <div className={"mt-[30px] max-w-6xl mx-auto px-[30px]"}>
                 <ToastContainer />
                 <Routes>
                     <Route path={"/"} element={<Home />} />
                     <Route path={"/login"} element={<Login />} />
                     <Route path={"/register"} element={<Register />} />
+                    <Route path={"/unauthorized"} element={<AccessDenied />} />
+
                     <Route path={"/profile"} element={<ProtectedRoute />}>
                         <Route path={"/profile"} element={<UserProfile />} />
                     </Route>
-                    <Route path={"/new-ticket"} element={<ProtectedRoute />}>
-                        <Route path={"/new-ticket"} element={<NewTicket />} />
+
+                    <Route path={"/users"} element={<ProtectedRoute role={true} />}>
+                        <Route path={"/users"} element={<Users />} />
                     </Route>
+
                     <Route path={"/tickets"} element={<ProtectedRoute />}>
                         <Route path={"/tickets"} element={<Tickets />} />
                     </Route>
+
+                    <Route path={"/new-ticket"} element={<ProtectedRoute />}>
+                        <Route path={"/new-ticket"} element={<NewTicket />} />
+                    </Route>
+
                     <Route path={"/tickets/:ticketId"} element={<ProtectedRoute />}>
                         <Route path={"/tickets/:ticketId"} element={<Ticket />} />
                     </Route>
