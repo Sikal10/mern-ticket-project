d67e8d33f812919b650adc5de4ff2e5b5fde6661
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-21 12:52:01 +0100

    User slice and reducer

--- client/src/redux/app/store.js
+++ client/src/redux/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice/authSlice";
 import ticketReducer from "../slices/ticketSlice/ticketSlice";
+import userReducer from "../slices/userSlice/userSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        user: userReducer,
         ticket: ticketReducer
     }
 });
