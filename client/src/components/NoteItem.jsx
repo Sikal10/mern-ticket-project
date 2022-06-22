28dbade039e6b9e09597c6a9d0793c449a7a9929
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-22 11:14:41 +0100

    Modal and NoteItem components

--- client/src/components/NoteItem.jsx
+++ client/src/components/NoteItem.jsx
@@ -5,7 +5,7 @@ import {selectAuthUser} from "../redux/slices/authSlice/authSlice";
 const NoteItem = ({note}) => {
     const {user} = useSelector(selectAuthUser);
     return (
-        <div className={"flex rounded-md bg-slate-100 border mt-4 justify-between px-3 py-3"}>
+        <div style={{background: note.isStaff && "rgba(0,0,0,.7)", color: note.isStaff && "white"}} className={"flex rounded-md bg-slate-100 border mt-4 justify-between px-3 py-3"}>
             <div>
                 <h2 className={"font-semibold text-[18px]"}>Note from {note.isStaff ? <span>Staff</span> : <span>{user.name}</span>}</h2>
                 <p>{note.note}</p>
