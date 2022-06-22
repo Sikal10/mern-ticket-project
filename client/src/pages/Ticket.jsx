4b43056311b05f33c45e6105e4e84aeded9b15f8
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-22 10:58:14 +0100

    Add notes functionality

--- client/src/pages/Ticket.jsx
+++ client/src/pages/Ticket.jsx
@@ -1,14 +1,25 @@
 import BackButton from "../components/BackButton";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {selectUserTicket} from "../redux/slices/ticketSlice/ticketSlice";
 import Spinner from "../components/Spinner";
 import {toast} from "react-toastify";
 import {getTicket, closeTicket} from "../redux/slices/ticketSlice/ticketAPI";
 import {useParams, useNavigate} from "react-router-dom";
+import {selectTicketNotes} from "../redux/slices/noteSlice/noteSlice";
+import {getTicketNotes} from "../redux/slices/noteSlice/noteAPI";
+import NoteItem from "../components/NoteItem";
+import Modal from "../components/Modal";
 
 const Ticket = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+
+    const closeModal = () => setIsModalOpen(false);
+
     const {loading, errorMsg, ticket} = useSelector(selectUserTicket);
+    const {loading: isNotesLoading, notes} = useSelector(selectTicketNotes);
 
     /** get the ID from the url using useParams */
     const {ticketId} = useParams();
@@ -22,9 +33,11 @@ const Ticket = () => {
         }
 
         dispatch(getTicket(ticketId));
+        dispatch(getTicketNotes(ticketId));
+
     }, [ticketId, dispatch, errorMsg])
 
-    if (loading === "loading") {
+    if (loading === "loading" || isNotesLoading === "loading") {
         return <Spinner/>;
     }
 
@@ -53,6 +66,14 @@ const Ticket = () => {
                     <h2 className={"text-[18px] font-semibold"}>Description of issue</h2>
                     <p>{ticket.description}</p>
                 </div>
+
+                <h2 className={"font-bold text-[19px] my-2"}>Notes</h2>
+
+                {ticket.status !== "closed" && <button className={"bg-gray-600 text-white px-5 py-2 rounded-md"} onClick={openModal}> Add Note</button>}
+
+                {notes.map((note) => <NoteItem key={note._id} note={note} />)}
+
+                {isModalOpen && <Modal ticketId={ticketId} onCloseModal={closeModal} />}
 
                 {ticket.status !== "closed" && <button onClick={handleCloseTicket} className={"mt-5 w-full bg-red-700 text-white py-2 rounded-md border-0"}>Close Ticket</button>}
             </div>
--- client/src/redux/app/store.js
+++ client/src/redux/app/store.js
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice/authSlice";
 import ticketReducer from "../slices/ticketSlice/ticketSlice";
 import userReducer from "../slices/userSlice/userSlice";
+import noteReducer from "../slices/noteSlice/noteSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         user: userReducer,
-        ticket: ticketReducer
+        ticket: ticketReducer,
+        note: noteReducer
     }
 });
