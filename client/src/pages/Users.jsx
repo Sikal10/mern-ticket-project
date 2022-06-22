e66e6fcc935fe133f634e5dc39ccc5eb03456f75
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-22 03:07:28 +0100

    Users page

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,23 +1,43 @@
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {selectCurrentUser} from "../redux/slices/userSlice/userSlice";
 import {getAllUsers} from "../redux/slices/userSlice/userAPI";
 import {selectAuthUser} from "../redux/slices/authSlice/authSlice";
 import {toast} from "react-toastify";
+import Spinner from "../components/Spinner";
 
 
 const Users = () => {
-    const {isAdmin, loading, success} = useSelector(selectAuthUser);
-    const {errorMsg} = useSelector(selectCurrentUser);
+    const {loading} = useSelector(selectAuthUser);
+    const {errorMsg, users} = useSelector(selectCurrentUser);
     const dispatch = useDispatch();
 
     useEffect(() => {
+        if (errorMsg) {
+            toast.error(errorMsg)
+        }
+
         dispatch(getAllUsers());
-    }, []);
+    }, [errorMsg]);
+
+    if (loading === "loading") return <Spinner />
 
     return (
         <div>
-            This is a page for every user
+            <div className={"user-list-heading"}>
+                <div>ID</div>
+                <div>Name</div>
+                <div>Email</div>
+                <div>isAdmin</div>
+            </div>
+
+            {users.map((user) => <div className={"user-list"} key={user._id}>
+                <p>{user._id}</p>
+                <p>{user.name}</p>
+                <p>{user.email}</p>
+                {user.isAdmin ? <p>Admin</p> : <p>User</p>}
+            </div>)}
+
         </div>
     );
 };
